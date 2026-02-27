56ef6381b37d7ffbca29267963e09d7443fc59a9
JuhiVathsalya001 <juhivathsalyakothapalli@gmail.com>
2026-02-27 22:00:57 +0530

    second

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 
+MONGO_URL="";
+
+main()
+    .then(()=>{
+        console.log("connected to DB");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get('/' ,(req,res)=>{
+    res.send("Hi i am root");
+});
+
 app.listen(8080,() =>{
     console.log("app is listening to port 8080");
 });
